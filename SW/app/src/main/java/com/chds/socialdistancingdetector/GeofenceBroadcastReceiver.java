9e26d06fefa767bc4866efc5b427a4db319cc9f3
Ibrahim Aly <ialy@ualberta.ca>
2021-04-08 00:49:19 -0600

    Geofencing kinda works

---
+++ SW/app/src/main/java/com/chds/socialdistancingdetector/GeofenceBroadcastReceiver.java
@@ -0,0 +1,44 @@
+package com.chds.socialdistancingdetector;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofenceStatusCodes;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.List;
+
+public class GeofenceBroadcastReceiver extends BroadcastReceiver {
+    // ...
+    public void onReceive(Context context, Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+            String errorMessage = GeofenceStatusCodes
+                    .getStatusCodeString(geofencingEvent.getErrorCode());
+            Log.e("error", errorMessage);
+            return;
+        }
+
+        // Get the transition type.
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        // Test that the reported transition was of interest.
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            // Get the geofences that were triggered. A single event can trigger
+            // multiple geofences.
+            List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+            Log.i("geofenceTransition", "Entered some geofence");
+
+        } else if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+            // Get the geofences that were triggered. A single event can trigger
+            // multiple geofences.
+            List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+
+            Log.i("geofenceTransition", "Exited some geofence");
+        }
+    }
+
+}
--- SW/app/src/main/java/com/chds/socialdistancingdetector/SettingsActivity.java
+++ SW/app/src/main/java/com/chds/socialdistancingdetector/SettingsActivity.java
@@ -1,14 +1,86 @@
 package com.chds.socialdistancingdetector;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
+    Switch geofenceSwitch;
+    Button setButton;
+    Button cancelButton;
+    Button saveButton;
+
+    Location selectedLocation;
+
+    LocationManager locationManager;
+
+    private Location getCurrentLocation() {
+        if (
+                ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED)
+        {
+            String[] PERMISSIONS = {
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            };
+
+            ActivityCompat.requestPermissions (
+                    SettingsActivity.this,
+                    PERMISSIONS,
+                    1
+            );
+
+            return null;
+        }
+
+        return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        geofenceSwitch = findViewById(R.id.geofence_switch);
+        setButton = findViewById(R.id.set_geofence_button);
+        cancelButton = findViewById(R.id.cancel_button);
+        saveButton = findViewById(R.id.save_button);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        setButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedLocation = getCurrentLocation();
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent output = new Intent();
+
+                if (geofenceSwitch.isChecked() && selectedLocation == null) {
+                    selectedLocation = getCurrentLocation();
+                }
+
+                output.putExtra("geofencingEnabled", geofenceSwitch.isChecked());
+                output.putExtra("selectedLat", selectedLocation.getLatitude());
+                output.putExtra("selectedLong", selectedLocation.getLongitude());
+                setResult(RESULT_OK, output);
+                finish();
+            }
+        });
     }
+
 }
